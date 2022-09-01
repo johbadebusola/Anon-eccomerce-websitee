256adba17d5b82ede03812d3075d63b6759e57d6
Adebusola John <johntoyin97@gmail.com>
2022-09-02 00:39:28 +0100

    edit 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,7 @@
 
 
-//   // breakpoints: {
-//   //   600:{
-//   //     slidesPerView: 2,
-//   //     spaceBetween: 10
-//   //   }
-//   // },
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//  
-
   var swiper1 = new Swiper(".mySwiper", {
-    
+    spaceBetween: 5,
     scrollbar: {
       el: ".swiper-scrollbar",
       hide: true,

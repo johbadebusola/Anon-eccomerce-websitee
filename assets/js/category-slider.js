750ff36932bad56ad0fff6ff9fb211e59ceb5c27
Adebusola John <johntoyin97@gmail.com>
2022-09-24 16:35:01 +0100

    update

--- assets/js/category-slider.js
+++ assets/js/category-slider.js
@@ -22,30 +22,4 @@ const swiper = new Swiper(".mySwiper2", {
     
     });
   
-    const swiper4 = new Swiper(".mySwiper4", {
-      // breakpoints: {
-      //   320:{
-      //     slidesPerView: 1,
-      //     spaceBetween: 5
-      //   },
-      //       570:{
-      //         slidesPerView: 2,
-      //         spaceBetween: 20
-      //       },
-      //       900:{
-      //         slidesPerView: 4,
-      //         spaceBetween: 10
-      //       }
-      //     },
-          
-          // And if we need scrollbar
-          scrollbar: {
-            el: ".swiper-scrollbar",
-            hide: true,
-          },
-          pagination: {
-            el: ".swiper-pagination"
-          }
-        
-        });
-      
+    
---
+++ assets/js/dealOfTheDay.js
@@ -0,0 +1,13 @@
+const swiper4 = new Swiper(".mySwiper4", {
+  
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+});
